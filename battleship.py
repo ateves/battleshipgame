389d524853671e5470b5a5b08c55c3e66a399a4f
ateves <39039918+ateves@users.noreply.github.com>
2024-03-13 06:01:52 -0400

    menu screen text added
    menu screen text, added

--- battleship.py
+++ battleship.py
@@ -2,6 +2,8 @@ from random import randint
 '''board statement/variable'''
 board = []
 
+print("Welcome to Single Player BattleShip by Anthony Teves! Try to find the ship to sink!")
+
 '''this for loop sets the range in place'''
 for x in range(0, 5):
   board.append(["O"] * 5)
@@ -33,9 +35,9 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 
 '''print statement for above'''
-print(ship_row + 1)
+'''print(ship_row + 1)
 print(ship_col + 1)
-
+'''
 '''print statements for column and boards
 print(random_col(board))
 print(random_row(board))
@@ -45,16 +47,17 @@ guess_row = int(input("Guess Row: "))-1
 guess_col = int(input("Guess Col: "))-1
 
 not_hit=True
+
 while not_hit:
   '''This is a for loop accessing a guess'''
   if guess_row == ship_row and guess_col == ship_col:
     print("Congratulations! You sank my battleship!")
     not_hit=False
   else: 
-    print("you missed my battleship!")
+    print("You missed my battleship!")
     board[guess_row][guess_col] = "X"
     print_board(board)
-    guess_row = int(input("Guess Row: "))-1
-    guess_col = int(input("Guess Col: "))-1
+    guess_row = int(input("Guess Row? (1-5)"))-1
+    guess_col = int(input("Guess Column? (1-5)"))-1
 
 '''This Program Was Written By ANTHONY T'''
